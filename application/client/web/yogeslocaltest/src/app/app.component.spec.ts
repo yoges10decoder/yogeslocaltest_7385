828a7089cc2d201e5ea67956994e5f295733615e
yoges10decoder <yogeskumar@10decoders.in>
2022-05-02 13:26:30 +0000

    code commited from geppetto!

---
+++ application/client/web/yogeslocaltest/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'yogeslocaltest'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('yogeslocaltest');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('yogeslocaltest app is running!');
+  });
+});
---
+++ application/client/web/yogeslocaltest/src/shared/shared.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Injectable()
+export class SharedService {
+
+     public WEB_API = environment.WEB_API;
+     public UPLOAD_API = environment.UPLOAD_API;
+    public MOBILE_API = environment.MOBILE_API;
+    
+}
+  
